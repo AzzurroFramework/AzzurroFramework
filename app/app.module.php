6c4ec09fa74ea1aa49e1b4e4d8235aaee6eb1fef
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2019-01-02 12:29:34 +0100

    Refactored index.php, and user app files
    - Refactored index.php
    - Renamed app.php into app.module.php
    - Framework will load app/autoloader.php

--- app/app.module.php
+++ app/app.module.php
@@ -3,15 +3,17 @@
 	User application main file
 
 	This file is the main file of your application.
-	Inside this file you can develop your application or call an autoloader.
+	Inside this file you can develop your main application module
 
 	DO NOT DELETE THIS FILE otherwise your application will not be loaded.
 */
 
 	// Application main module
 	$azzurro
-		->app("app", [])
+		->app("app", [
+
+		])
 
 		->run(function () {
-			echo "APP RUNNED!";
+			echo "AzzurroFramework runned!";
 		});
